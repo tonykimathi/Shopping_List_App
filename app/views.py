f22a9abcf99e85f8678dc5a3d63f597f80f5ce81
tonykimathi <kimathitony93@gmail.com>
2017-09-01 06:44:20 +0300

    [FEATURE] Add login and logout feature in flask

--- app/views.py
+++ app/views.py
@@ -1,21 +1,47 @@
-from flask import render_template
+from flask import render_template, session, redirect, url_for, flash
+from functools import wraps
 from run import app
 
 
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
 
     return render_template('index.html')
 
 
-@app.route('/Sign Up')
+@app.route('/Sign Up', methods=['POST'])
 def sign_up():
 
     return render_template('Sign Up.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    return render_template('login.html')
 
 
 @app.route('/dashboard')
 def dashboard():
 
     return render_template('dashboard.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+        session.pop('logged_in', None)
+        flash('You were logged out!')
+        return redirect(url_for('login'))
